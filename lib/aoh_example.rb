cea2f029eb644ac35a6deb46352d69803f040537
Gustave Habimana <ghabiman@gmail.com>
2020-02-14 21:45:59 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -15,7 +15,9 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  literal_aoh = 
+  literal_aoh = {
+    name: ""
+  }
 end
 
 def aoh_lookup(aoh, row, key)
